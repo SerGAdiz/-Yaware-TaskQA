9c78da031151be6c22d0cf1e82c635162bc3cebe
Serhii <SEROGA9090@ukr.net>
2023-03-14 23:13:55 +0200

    GoalListWithTimer_app

---
+++ GoalListWithTimep/src/main/java/org/example/constant/Name.java
@@ -0,0 +1,5 @@
+package org.example.constant;
+
+public class Name {
+    public static final String APP_NAME = "Goal list with timer";
+}
---
+++ GoalListWithTimep/src/main/java/org/example/window/MainWindowApp.java
@@ -0,0 +1,12 @@
+package org.example.window;
+
+import javax.swing.*;
+
+public class MainWindowApp {
+    public JPanel mainWin;
+    public JPanel DownPanel;
+    public JButton createButton;
+    public JButton button2;
+    public JButton button3;
+    public JButton chooseButton;
+}
